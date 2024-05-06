5320134caccec5ae92f2352dc684d2357e04e356
Alex L <34932547+alexlazarian@users.noreply.github.com>
2024-05-05 17:56:55 -0700

    Initial Commit

---
+++ Zartonk/ZartonkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZartonkApp.swift
+//  Zartonk
+//
+//  Created by Six Moog on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ZartonkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZartonkUITests/ZartonkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ZartonkUITestsLaunchTests.swift
+//  ZartonkUITests
+//
+//  Created by Six Moog on 5/5/24.
+//
+
+import XCTest
+
+final class ZartonkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
